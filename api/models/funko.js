a26c56d8ffc19922232e7b762f67d34b73fbdc76
sebastian <boly1997@hotmail.com>
2021-01-19 20:16:37 +0100

    Last one

--- api/models/funko.js
+++ api/models/funko.js
@@ -8,8 +8,8 @@ const funkoSchema = new Schema({
     "category": { type: String, required: true },
     "franchise": { type: String, required: true },
     "retailPrice": { type: Number, required: true },
-    "actualValue": { type: Number, required: true }, 
-    "exclusive": { type: Boolean, required: true, default: false }, 
+    "actualValue": { type: Number, required: true },
+    "exclusive": { type: Boolean, required: true, default: false },
     "exclusivity": { type: String, required: false, default: "" },
     "discontinued": { type: Boolean, required: false, default: false },
     "image": { type: String, required: false, default: "" }
