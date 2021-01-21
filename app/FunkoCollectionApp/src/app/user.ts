359fc49a9133c849df317f0a1a6d701fff5e9162
israeltq <israeltrujilloq96@gmail.com>
2021-01-21 00:54:09 -0800

    [Issue #10] Implemented Create New User Component

---
+++ app/FunkoCollectionApp/src/app/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    userName: string;
+    email: string;
+    birthday: string;
+    firstName: string;
+    lastName: string;
+    funkoCollection: string[];
+    wishList: string[];
+}
--- app/FunkoCollectionApp/src/environments/environment.ts
+++ app/FunkoCollectionApp/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  serverPort: 3000,
+  serverUrl: 'http://localhost',
+  serverRoute: 'api/'
 };
 
 /*
