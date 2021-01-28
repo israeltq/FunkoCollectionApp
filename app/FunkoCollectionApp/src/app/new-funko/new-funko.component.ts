2878058efe6e54cd64c6a97ff2f63c650eabbad0
sebastian <boly1997@hotmail.com>
2021-01-28 04:43:16 +0100

    [Issue #11] Implemented Create New Funko Component

---
+++ app/FunkoCollectionApp/src/app/new-funko/new-funko.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Funko } from '../funko';
+import { FunkoService } from '../funko.service';
+import { NotificationsService, NotificationType } from '../notifications.service';
+
+@Component({
+  selector: 'app-new-funko',
+  templateUrl: './new-funko.component.html',
+  styleUrls: ['./new-funko.component.css']
+})
+
+export class NewFunkoComponent implements OnInit {
+  newFunko: Funko;
+  funkoForm: FormGroup;
+
+  constructor(
+    private funkoService: FunkoService,
+    private notificationsService: NotificationsService
+  ) { }
+
+  ngOnInit(): void {
+    this.funkoForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      description: new FormControl(''),
+      category: new FormControl('', [Validators.required]),
+      franchise: new FormControl('', [Validators.required]),
+      retailPrice: new FormControl(10.99, [Validators.required]),
+      actualValue: new FormControl(0, [Validators.required]),
+      exclusive: new FormControl(false, [Validators.required]),
+      exclusivity: new FormControl('', [Validators.required]),
+      discontinued: new FormControl(false, [Validators.required])
+    });
+  }
+
+  hasError(controlName: string, errorName: string): boolean {
+    return this.funkoForm.controls[controlName].hasError(errorName);
+  }
+
+  createFunko(funkoFormValue: any): void {
+    if (this.funkoForm.valid) {
+      this.newFunko = {
+        name: funkoFormValue.name,
+        description: funkoFormValue.description,
+        category: funkoFormValue.category,
+        franchise: funkoFormValue.franchise,
+        retailPrice: funkoFormValue.retailPrice,
+        actualValue: funkoFormValue.actualValue,
+        exclusive: funkoFormValue.exclusive,
+        exclusivity: funkoFormValue.exclusivity,
+        discontinued: funkoFormValue.discontinued,
+        image: funkoFormValue.image
+      };
+
+      this.funkoService.createNewFunko(this.newFunko).subscribe((res) => {
+        this.notificationsService.sendMessage({
+          message: `Funko: ${res.name} succesfully created!`,
+          type: NotificationType.success
+        });
+      });
+    }
+  }
+
+}
+
